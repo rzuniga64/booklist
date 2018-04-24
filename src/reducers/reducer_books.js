8dc611ce5283abf24f8255fa4b6f0336e91fba6a
Raul Zuniga <rzuniga64@gmail.com>
2018-04-24 00:00:26 -0500

    Don't use colon in name of book

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -4,7 +4,7 @@
  */
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Javascript, The Good Parts', pages: 101 },
     { title: 'Harry Potter', pages: 39 },
     { title: 'The Dark Tower', pages: 85 },
     { title: 'Eloquent Ruby', pages: 1 }
